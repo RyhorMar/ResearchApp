41cd18777ca815a9aa8300458177f991ba6cbe44
Grigorii Martsynkevich <gmartsynkevich@scnsoft.com>
2024-10-25 07:18:40 +0300

    Initial commit

---
+++ reseacrhapp/reseacrhapp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  reseacrhapp
+//
+//  Created by Martsynkevich, Grigorii on 10/25/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ reseacrhapp/reseacrhapp/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  reseacrhapp
+//
+//  Created by Martsynkevich, Grigorii on 10/25/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
