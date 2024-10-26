9f77d2deed22d678a978d29880f6c9fd6fe4d145
Grigorii Martsynkevich <gmartsynkevich@scnsoft.com>
2024-10-26 15:00:48 +0300

    Added ResearchKit

---
+++ reseacrhapp/reseacrhapp/Screens/Home/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  reseacrhapp
+//
+//  Created by Martsynkevich, Grigorii on 10/26/24.
+//
+import UIKit
+import ResearchKit
+
+final class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureSteps()
+    }
+    
+    private func configureSteps() {
+
+
+    }
+}
